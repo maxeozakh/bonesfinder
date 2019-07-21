cd2c880b97b4c060fffce778383fef616ca60813
mmmaxeo <walkworksleep@gmail.com>
2019-07-21 14:09:23 +0300

    Add description on first level

--- src/App.js
+++ src/App.js
@@ -7,6 +7,39 @@ function App() {
   return (
     <>
       <div id="game">
+        {currentStep === 0 && (
+          <>
+            <span hidden={currentStep !== 0} class="description">
+              Drag skeleton
+            </span>
+            <div className="icons-credentials">
+              Icons made by{' '}
+              <a href="https://www.freepik.com/" title="Freepik">
+                Freepik
+              </a>{' '}
+              and{' '}
+              <a
+                href="https://www.flaticon.com/authors/eucalyp"
+                title="Eucalyp"
+              >
+                Eucalyp
+              </a>{' '}
+              from{' '}
+              <a href="https://www.flaticon.com/" title="Flaticon">
+                www.flaticon.com
+              </a>{' '}
+              is licensed by{' '}
+              <a
+                href="http://creativecommons.org/licenses/by/3.0/"
+                title="Creative Commons BY 3.0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CC 3.0 BY
+              </a>
+            </div>
+          </>
+        )}
         <Game
           key={currentStep}
           currentStep={currentStep}
@@ -14,29 +47,6 @@ function App() {
         />
       </div>
       <br />
-      <div className="icons-credentials">
-        Icons made by{' '}
-        <a href="https://www.freepik.com/" title="Freepik">
-          Freepik
-        </a>{' '}
-        and{' '}
-        <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">
-          Eucalyp
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>{' '}
-        is licensed by{' '}
-        <a
-          href="http://creativecommons.org/licenses/by/3.0/"
-          title="Creative Commons BY 3.0"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CC 3.0 BY
-        </a>
-      </div>
     </>
   );
 }
